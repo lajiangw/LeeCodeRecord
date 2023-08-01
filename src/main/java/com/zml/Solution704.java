e3842b222d0d0d906be2225e7d1616332b259669
zml <320961101@qq.com>
2023-08-01 21:28:52 +0800

    力扣  二分算法

--- src/main/java/com/zml/Solution704.java
+++ src/main/java/com/zml/Solution704.java
@@ -6,6 +6,11 @@ package com.zml;
  * @date 2023-07-31 20:59
  */
 public class Solution704 {
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{1, 2, 3, 4, 5, 6, 77, 88, 99}, 77));
+
+    }
+
     /**
      * 二分查找
      *
@@ -13,11 +18,11 @@ public class Solution704 {
      * @param target
      * @return
      */
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while (left >= right) {
-            int mid = (right + left) / 2 + left;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
             int num = nums[mid];
             if (num == target) {
                 return mid;
@@ -28,4 +33,22 @@ public class Solution704 {
         }
         return -1;
     }
+
+    public int test(int[] arr, int t) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int num = arr[mid];
+            if (t == num) {
+                return mid;
+            } else if (t > num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
