248f1b236d9a174be9a45f36b70818051c73db70
zml <320961101@qq.com>
2023-08-15 23:56:09 +0800

    力扣 387. 字符串中的第一个唯一字符

--- src/main/java/com/zml/Solution387.java
+++ src/main/java/com/zml/Solution387.java
@@ -10,10 +10,10 @@ import java.util.HashSet;
  */
 public class Solution387 {
     public static void main(String[] args) {
-
+        new Solution387().firstUniqChar("aaabs");
     }
 
-//    因为只会出现字母，所以创建一个26的数组代表26字母，出现一次就进行+1
+    //    因为只会出现字母，所以创建一个26的数组代表26字母，出现一次就进行+1
 //    然后再次遍历s字符串，一次判断只出现一次的字符串，然后进行返回即可。
     public int firstUniqChar(String s) {
         int[] arr = new int[26];
