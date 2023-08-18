adb9097107b316bcd4ebcab34966ee9b7b4ca354
zml <320961101@qq.com>
2023-08-19 00:03:54 +0800

    力扣 844. 比较含退格的字符串

--- src/main/java/com/zml/Solution844.java
+++ src/main/java/com/zml/Solution844.java
@@ -1,5 +1,7 @@
 package com.zml;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Objects;
 
 /**
@@ -11,25 +13,35 @@ public class Solution844 {
 
     public static void main(String[] args) {
         Solution844 solution844 = new Solution844();
-        System.out.println(solution844.backspaceCompare("ab#c", "ad#c"));
+
+        String s1 = "ab##c#";
+        String s2 = "ad##c#";
+
+        System.out.println(solution844.backspaceCompare(s1, s2));
 
     }
 
     //给定 s 和 t 两个字符串，当它们分别被输入到空白的文本编辑器后，如果两者相等，返回 true 。# 代表退格字符。
     public boolean backspaceCompare(String s, String t) {
+        if (s.isEmpty() && t.isEmpty()) {
+            return true;
+        }
         s = txEdit(s);
         t = txEdit(t);
         return Objects.equals(s, t);
     }
 
-    public String txEdit(String s) {
+    //    使用两个指针，slow 代表编辑器中的下标，遇到退格则－1 不是退格的话则会将值覆盖。
+    public String txEdit(@NotNull String s) {
         char[] charArray = s.toCharArray();
         int len = charArray.length;
         int slow = 0;
         for (int i = 0; i < len; i++) {
+//             slow代表在文本编辑器的下标，遇到退格则 -1 ，然后此次循环结束 i++
             if (charArray[i] == '#') {
                 slow = slow == 0 ? 0 : slow - 1;
             } else {
+//                如果不是退格则将i下标的值赋值给slow，如果之前遇到退格，则会吧退格需要删除的值删覆盖掉。
                 charArray[slow] = charArray[i];
                 slow++;
             }
