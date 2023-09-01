7dd1a61b9f8de147e73c6da467801ee08cbf49e5
zml <320961101@qq.com>
2023-09-01 22:31:11 +0800

    力扣 226. 翻转二叉树

--- src/main/java/com/zml/Solution226.java
+++ src/main/java/com/zml/Solution226.java
@@ -8,7 +8,25 @@ import com.zml.bean.TreeNode;
  * @date 2023-08-30 20:45
  */
 public class Solution226 {
-    public TreeNode invertTree(TreeNode root) {
 
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        // 保存右子树
+        TreeNode rightTree = root.right;
+        // 交换左右子树的位置
+        root.right = invertTree(root.left);
+        root.left = invertTree(rightTree);
+        return root;
     }
+
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+        invertTree2(root.left);
+        TreeNode right = root.right;
+        root.right = root.left;
+        root.left = right;
+        invertTree2(root.left);
+        return root;
+    }
+
 }
