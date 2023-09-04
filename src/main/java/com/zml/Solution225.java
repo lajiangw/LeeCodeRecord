c47882ba11819d6d0ec6d234b6133aa13ff02ade
zml <320961101@qq.com>
2023-09-04 20:28:42 +0800

    力扣 225. 模拟栈

---
+++ src/main/java/com/zml/Solution225.java
@@ -0,0 +1,77 @@
+package com.zml;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * @author ZhangMinlei
+ * @description
+ * @date 2023-09-04 18:30
+ */
+public class Solution225 {
+
+}
+
+class MyStack {
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+
+
+    public MyStack() {
+        queue1 = new LinkedList<Integer>();
+        queue2 = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    public int pop() {
+        return queue1.poll();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+class MyStack2 {
+    Queue<Integer> queue;
+
+    public MyStack2() {
+        queue = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        int n = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < n; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
