b02a90c51cfec07a3c8d7afc6444e1b5f24cf228
zml <320961101@qq.com>
2023-09-12 21:04:39 +0800

    力扣 203. 移除链表元素

---
+++ src/main/java/com/zml/Solution203.java
@@ -0,0 +1,59 @@
+package com.zml;
+
+import java.util.List;
+
+/**
+ * @author ZhangMinlei
+ * @description 203. 移除链表元素
+ * @date 2023-09-12 19:56
+ */
+public class Solution203 {
+    public ListNode removeElements(ListNode head, int val) {
+//         第一个循环的作用是删除链表头部所有值等于给定值 val 的节点。
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null) {
+            return head;
+        }
+
+        ListNode listNode = head;
+        while (listNode.next != null) {
+            if (listNode.next.val == val) {
+                listNode.next = listNode.next.next;
+            } else {
+                listNode = listNode.next;
+            }
+        }
+        return head;
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+
+        ListNode listNode = new ListNode(val - 1);
+        listNode.next = head;
+        ListNode prev = listNode;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return listNode.next;
+    }
+
+    //    递归
+    public ListNode removeElements3(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        head.next = removeElements3(head.next, val);
+        if (head.val == val) {
+            return head.next;
+        } else {
+            return head;
+        }
+    }
+
+}
